b5b37a0f323737ae3ccdfcfc21796cf06b42f557
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-12-03 14:31:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    //define an array with unknown elements
+    int[] arr1 = new int[5];
+    //define an array with known elements
+    int[] arr2 = {45,46,47,48,49};
+
+    int sum;
+    sum = 3;
+
+    arr1[0] = 4;
+    arr1[1] = arr2[1];
+  }
+}
