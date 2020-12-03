c86931a7cf23cda5524f39c023f764ceb5f04cf1
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-12-03 15:28:29 +0000

    e

--- Main.java
+++ Main.java
@@ -1,14 +1,24 @@
 class Main {
   public static void main(String[] args) {
     //define an array with unknown elements
-    int[] arr1 = new int[5];
+    int[] arr1 = new int[7];
     //define an array with known elements
-    int[] arr2 = {45,46,47,48,49};
+    int[] arr2 = {51,52,53,54,55};
 
     int sum;
     sum = 3;
 
     arr1[0] = 4;
     arr1[1] = arr2[1];
+    System.out.println(arr1[3]);
+    System.out.println(arr1[4]);
+
+    for(int index = 2; index < arr2.length; index++){
+      arr1[index] = arr2[index];
+      System.out.println(arr1[index]);
+    }
+    arr1[4] = 6;
+    arr1[2] = 7;
+    System.out.println(arr1[4] +" "+ arr1[2]);
   }
 }
